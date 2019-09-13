73878e9f146cae8809f1fc6cb59d9016a2cc366c
Teejay Obazee <forum.providence@yahoo.com>
2019-09-13 22:25:20 +0800

    Class bindings

--- vue-cart/main.js
+++ vue-cart/main.js
@@ -4,19 +4,32 @@ var app = new Vue({
     product: 'Shoe',
     image: 'shoe-red.jpg',
     inStock: false,
-    inventory: 11,
+    inventory: 0,
     onSale: false,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
-        variantColor: "red"
+        variantColor: "red",
+        variantImage: 'shoe-red.jpg'
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: 'shoe-blue.jpg'
       }
     ],
     cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart: function() {
+      this.cart = 0;
+    }
   }
 })
