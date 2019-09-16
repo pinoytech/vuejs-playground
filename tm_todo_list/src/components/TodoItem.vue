b25f59729e7192363826681d752eaac5e7143773
Teejay Obazee <64101+pinoytech@users.noreply.github.com>
2019-09-16 20:20:52 +0800

    Creating a simple Todo List

---
+++ tm_todo_list/src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-item" :class="{ 'is-complete':todo.completed }">
+    <p>
+      <input type="checkbox" @change="updateTask" :checked="todo.completed">
+      {{ todo.title }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    updateTask() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px solid dotted;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #fF0000;
+    color: #FFFFFF;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
---
+++ tm_todo_list/src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem :todo="todo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+}
+</script>
+
+<style scoped>
+ 
+</style>
