5ce9f9dbf3ed6002eb4cf052db6e7b4dde2942e9
Teej.PinoyTech <64101+pinoytech@users.noreply.github.com>
2019-10-17 15:15:26 +0800

    Normal Vue Todo List

---
+++ vue-composition-api-todo-list/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="(item, index) in todos" :key="index">{{ item }}</li>
+    </ul>
+    <input v-model="todo" />
+    <button @click="addTodo">Add Todo Item</button>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      todo: "",
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.unshift(this.todo);
+      this.todo = "";
+    }
+  }
+};
+</script>
+
+<style></style>
